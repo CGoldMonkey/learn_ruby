0a3a53c80542028b18fa369302f9b99175d62c3e
asteven <agstevens09@gmail.com>
2020-01-06 16:43:27 -0600

    Redid simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,4 +1,72 @@
 #write your code here
+def echo(word)
+  return word.to_s
+end
+
+def shout(word)
+  return word.upcase
+end
+
+def repeat(word, number = 2)
+  words = []
+  number.times { words.push(word) }
+  return words.join(' ')
+end
+
+def start_of_word(word, length)
+  return word.slice(0, length)
+end
+
+def first_word(text)
+  return text.split(' ').first
+end
+
+def titleize(words)
+  word_list = words.split.each do |word|
+    if (word != 'and' && word != 'the' && word != 'over')
+      word.capitalize!
+    end
+  end
+  word_list[0].capitalize!
+  word_list.join(' ')
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
 def echo(text)
   text = text.to_s
   return text
@@ -37,3 +105,5 @@ def titleize(text)
   words[0].capitalize!
   return words.join(" ")
 end
+
+=end
