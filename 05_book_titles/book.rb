2f65d3eaf2f70a99cb4502c5fad9831d13c8e04b
asteven <agstevens09@gmail.com>
2020-01-08 12:09:19 -0600

    Redid the Book Title Exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,4 +1,27 @@
 class Book
+# write your code here
+  attr_accessor :title
+
+  def title
+	conjunctions = 
+	words = @title.split(' ')
+	p words
+	words.map! do |word|
+	  if (word.match?(/^and(?!.)|^in(?!.)|^the(?!.)|^of(?!.)|^a(?!.)|^an(?!.)/))
+        #do nothing
+	  else
+		word.capitalize!
+	  end
+	  word	
+	end
+	words[0].capitalize!
+	return words.join(' ')
+  end
+end
+
+
+=begin
+class Book
   attr_accessor :title
 
   def title
@@ -16,3 +39,5 @@ class Book
   	words.join(' ')
   end
 end
+
+=end
